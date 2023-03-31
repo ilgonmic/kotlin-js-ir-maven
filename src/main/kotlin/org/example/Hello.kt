3f919343f4a12e317d2a0208a375c655704fae6f
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-03-31 14:51:29 +0200

    Initial commit

---
+++ src/main/kotlin/org/example/Hello.kt
@@ -0,0 +1,14 @@
+package org.example
+
+import kotlinx.coroutines.GlobalScope
+
+/**
+ * Example main function. Started at script body.
+ * At first time you have to run `mvn package`. 
+ * Open example index.html in browser once you compile it.
+ */
+fun main(args: Array<String>) {
+    GlobalScope // just reference to dependency declaration
+    println("Hello, world")
+}
+
